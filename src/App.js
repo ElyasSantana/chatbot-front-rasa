ae83f64b0e987169d93df53aefc9d7890d0f28b5
ElyasPicpay <elyas.santana@picpay.com>
2021-10-20 13:58:36 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import Widget from 'rasa-webchat';
+import { useEffect } from 'react';
 import './App.css';
 
-function App() {
+function CustomWidget() {
+  const socketUrl = process.env.REACT_APP_SOCKET_URL;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Widget
+      initPayload={'oi'}
+      socketUrl={socketUrl}
+      customData={{ language: 'pt-br' }}
+      title={'PicPay'}
+      subtitle={'Assistente Virtual'}
+      inputTextFieldHint={'Digite sua mensagem...'}
+      showCloseButton={false}
+      fullScreenMode={true}
+      params={{ storage: 'session' }}
+    />
+  );
+}
+
+function App() {
+  useEffect(() => {
+    let count = 0;
+
+    const loop = setInterval(() => {
+      let buttonOpenChat = document.getElementsByClassName('rw-launcher');
+      let chatOpen = document.getElementsByClassName(
+        'rw-conversation-container'
+      );
+
+      if (buttonOpenChat.length > 0 && chatOpen.length === 0) {
+        buttonOpenChat[0].click();
+        clearInterval(loop);
+      }
+
+      if (count === 5) clearInterval(loop);
+
+      count++;
+    }, 500);
+  });
+
+  return (
+    <div className='App'>
+      <CustomWidget />
     </div>
   );
 }
